9b339cd8e5264e9ab44a6805b942d029a4092997
ASHISH KUMAR <114363439+ashishnayak6488@users.noreply.github.com>
2024-08-07 22:49:38 +0530

    update frontend URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://portfolio-frontend-mauve-psi.vercel.app/",
+      "https://portfolio-frontend-mauve-psi.vercel.app",
       "http://localhost:3000",
     ],
     credentials: true,
